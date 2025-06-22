8cc1e859660e97652918faccb7076a01bba6b803
cristina223 <cristina223@gmail.com>
2025-06-22 18:28:23 -0400

    monthly cleanup

---
+++ git-v2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+git rm -r --cached .
+cd x86_64
+rm repo.db repo.db.tar.gz repo.files repo.files.tar.gz
+repo-add repo.db.tar.gz *.pkg.tar.zst
+
+cd ..
+
+#repoctl update -P repo
+#repoctl update -P test
+
+
+
+
+# checking if I have the latest files from github
+echo "Checking for newer files online first"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Write your commit comment!"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -f -u origin main
+
+echo "################################################################"
+echo "###################    Git Push Done      ######################"
+echo "################################################################"
